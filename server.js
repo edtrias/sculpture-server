9772263e780c62bc8ee8f88c9d9000ced09a9896
Eduard Trias <triaseduard@gmail.com>
2017-08-15 18:42:06 +0200

    add crud users

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const users = require('./routes/users')
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/kraszani', {
   useMongoClient: true
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/api/admin', users)
+
 app.get('/', (req, res) => {
   res.send('hola')
 });
+
 
 app.listen(27017, () => {
   console.log('Listening on port 27017')
