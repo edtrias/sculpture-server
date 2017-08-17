6398ef2d67ca6b1e96bcdeeb56d7673616ac2783
Alex Diaz <diaz.infante@gmail.com>
2017-08-17 15:13:25 +0200

    CRUD artowork

--- routes/artworks.js
+++ routes/artworks.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const multer = require('multer');
 
 const Artwork = require('../models/artwork');
 
 const router = express();
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads/artworks')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
 
 router.get('/artworks', (req, res) => {
   Artwork.find()
@@ -19,32 +29,65 @@ router.get('/artworks/:id', (req, res) => {
 })
 
 router.post('/artworks', (req, res) => {
-  Artwork.create({
-    name: req.body.name,
-    family: req.body.family,
-    material: req.body.material,
-    //image: req.file...
-    specs: req.body.specs
+
+  let upload = multer({storage: storage}).single('image');
+
+  upload(req, res,(err) => {
+
+    Artwork.create({
+      name: req.body.name,
+      family: req.body.family,
+      material: req.body.material,
+      specs: req.body.specs,
+      image: req.file,
+      fieldname: req.file.fieldname,
+      originalname: req.file.originalname,
+      encoding: req.file.encoding,
+      mimetype: req.file.mimetype,
+      destination:req.file.destination,
+      filename: req.file.filename,
+      path: req.file.path,
+      size: req.file.size,
+    })
+      .then(data => res.json({response: data , message: 'Artwork has been created.'}))
+      .catch(error => res.json(error))
   })
-    .then(data => res.json(data))
-    .catch(error => res.json(error))
-})
+});
 
 router.put('/artworks/:id', (req, res) => {
   Artwork.findOneAndUpdate({
     name: req.body.name,
     family: req.body.family,
     material: req.body.material,
-    //image: req.file...
     specs: req.body.specs
   })
-    .then(data => res.json(data))
-    .catch(error => res.json(error))
-})
+    .then(() => res.json({message: 'Artwork data updated successfully.'}))
+    .catch(error => res.json(error));
+});
+
+router.put('/artworks/:id/img'), (req, res) => {
+
+  var upload = multer({storage:storage}).single('image')
+  upload(req,res, (err) => {
+    Artwork.findOneAndUpdate({
+      image: req.file,
+      fieldname: req.file.fieldname,
+      originalname: req.file.originalname,
+      encoding: req.file.encoding,
+      mimetype: req.file.mimetype,
+      destination:req.file.destination,
+      filename: req.file.filename,
+      path: req.file.path,
+      size: req.file.size,
+    })
+     .then(() => res.json({message: 'Artwork image has been updated.'}))
+      .catch(error => res.json(error))
+  });
+};
 
 router.delete('/artworks/:id', (req, res) => {
   Artwork.findOneAndRemove()
-    .then(() => res.json({message: 'Removed successfully'}))
+    .then(() => res.json({message: 'Artwork Removed successfully.'}))
     .catch(error => res.json(error))
 })
 
--- routes/expos.js
+++ routes/expos.js
@@ -11,7 +11,7 @@ const router = express();
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads')
+    cb(null, 'public/uploads/expos')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
@@ -41,7 +41,7 @@ router.post('/expos', (req, res) => {
   // console.log(req.file);
   // res.status(204).end();
 
-  var upload = multer({storage: storage}).single('image')
+  let upload = multer({storage: storage}).single('image')
 
   upload(req, res, (err) => {
     Expo.create({
@@ -81,7 +81,9 @@ router.put('/expos/:id', (req, res) => {
     startDate: req.body.startDate,
     endDate: req.body.endDate,
     other: req.body.other
-  }).then(expo => res.json(expo));
+  })
+    .then(expo => res.json(expo))
+    .catch(error => res.json(error));
 });
 
 router.put('/expos/:id/img', (req, res) => {
