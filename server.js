1f1e016d792d09ee8dc684d1f0a881ca4beb7e0d
Eduard Trias <triaseduard@gmail.com>
2017-08-18 20:23:37 +0200

    add Access-Control-Allow-Methods for GET, POST, PUT and DELETE

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect('mongodb://localhost/kraszani', {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
