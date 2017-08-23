962becfe69d0575b6a427e23312e68bf3804f759
Alex Diaz <diaz.infante@gmail.com>
2017-08-23 10:11:08 +0200

    Add modifications in the  server for uploading files

--- routes/artworks.js
+++ routes/artworks.js
@@ -8,7 +8,7 @@ const Artwork = require('../models/artwork');
 const router = express();
 
 var storage = multer.diskStorage({
-  destination: (req, res, cb) => {
+  destination: (req, file, cb) => {
     cb(null, 'public/uploads/artworks')
   },
   filename: (req, file, cb) => {
@@ -33,7 +33,6 @@ router.post('/artworks', (req, res) => {
   let upload = multer({storage: storage}).single('image');
   
   upload(req, res,(err) => {
-
     Artwork.create({
       name: req.body.name,
       family: req.body.family,
@@ -42,12 +41,12 @@ router.post('/artworks', (req, res) => {
       image: req.file,
       fieldname: req.file.fieldname,
       originalname: req.file.originalname,
-      encoding: req.file.encoding,
-      mimetype: req.file.mimetype,
-      destination:req.file.destination,
-      filename: req.file.filename,
-      path: req.file.path,
-      size: req.file.size,
+     // encoding: req.file.encoding,
+      //mimetype: req.file.mimetype,
+      //destination:req.file.destination,
+      //filename: req.file.filename,
+      //path: req.file.path,
+      //size: req.file.size,
     })
       .then(data => res.json({response: data , message: 'Artwork has been created.'}))
       .catch(error => res.json(error))
